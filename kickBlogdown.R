d58008b26dd127fbf33414e3581c903b9df76a4b
takebayashi <take2_fmu@takebayashiyubu-no-MacBook-Air-2.local>
2020-04-19 23:00:29 +0900

    # upload supplemental materials

--- kickBlogdown.R
+++ kickBlogdown.R
@@ -1,5 +1,5 @@
 library(blogdown)
 
-blogdown::new_site(theme="zwbetz-gh/minimal-bootstrap-hugo-theme")
+#blogdown::new_site(theme="zwbetz-gh/minimal-bootstrap-hugo-theme")
 blogdown::build_site()
 blogdown::serve_site()
